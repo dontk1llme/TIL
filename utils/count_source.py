94e982463dd3888952eff7cd4eed9ea1286caf78
YeSeul <89983177+dontk1llme@users.noreply.github.com>
2023-01-16 14:58:47 +0900

    Update count_source.py

--- utils/count_source.py
+++ utils/count_source.py
@@ -41,8 +41,8 @@ def make_read_me(count_info):
 #### Python3
 {count_info}
 #### 아래의 페이지에서 제공하는 문제들로 구성되어 있습니다.
-[BaekJoon](https://www.acmicpc.net/)
-[Programmers](https://programmers.co.kr/)
+[BaekJoon](https://www.acmicpc.net/)  
+[Programmers](https://programmers.co.kr/)  
 [Samsung_SW_Academy](https://swexpertacademy.com/main/main.do)"""
 
 
