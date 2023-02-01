602a97830c043ce8c0c2e937e633fd95a6fa6aed
dontk1llme <20190525@sungshin.ac.kr>
2023-02-01 17:49:06 +0900

    230201

--- Python/algorithm/230201/13588-구간합.py
+++ Python/algorithm/230201/13588-구간합.py
@@ -13,3 +13,22 @@ for tc in range(1, T+1):
     print(f'#{tc} {max-min}')
 
 
+# 이렇게 하면 런타임 에러 날 수도 있음 (데이터 다 더해서 비교하니까 시간이 길어요)
+# 해결법: 1-4 더하면 다음 2-5를 다 더하는 게 아니라 1-4에서 2-4까지는 가져오고 5만 더해주기
+# 그러면 루프가 줄어용 아래 코드!!
+# T = int(input())
+# for test_case in range(1, T + 1):
+#     N, M = map(int, input().split())
+#     lst = list(map(int, input().split()))
+#     sm = 0
+#     for i in range(M):
+#         sm += lst[i]
+#     mn = mx = sm        # mn, mx값 초기화 필요
+#     for i in range(M, N):
+#         sm += lst[i]    # i위치 값 추가
+#         sm -= lst[i-M]  # i-M 위치 값 제거
+#         if mx<sm:
+#             mx=sm
+#         if mn>sm:
+#             mn=sm
+#     print(f'#{test_case} {mx-mn}')
---
+++ Python/algorithm/230201/13599-View.py
@@ -0,0 +1,16 @@
+# SWEA 1206
+
+T = 10
+for tc in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    around = [-2,-1,1,2] #주변건물 인덱스용
+    ans = 0
+    for i in range(2, N-2): #맨 왼쪽 두 칸과 맨 오른쪽 두 칸에 있는 건물은 항상 높이가 0
+        nl = [] #주변건물 높이
+        for j in around:
+            nl.append(arr[i-j]) #around 이용해서 저장해용
+        near = max(nl)
+        if arr[i] > near:
+            ans += arr[i]-near
+    print(f'#{tc} {ans}')
