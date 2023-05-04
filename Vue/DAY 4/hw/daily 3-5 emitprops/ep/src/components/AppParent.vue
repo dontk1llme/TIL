e82404f000ee646e1ba17c01b4faff394e83f017
dontk1llme <letm2go@gmail.com>
2023-05-04 12:50:21 +0900

    230504

---
+++ Vue/DAY 4/hw/daily 3-5 emitprops/ep/src/components/AppParent.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id='appParent'>
+    <h1>AppParent</h1>
+    <input v-model.number='InputNum2' type="number" @change='funcNum23'>
+    <p>appData:{{InputNum1}}</p>
+    <p>childData:{{InputNum3}}</p>
+    <AppChild
+      :InputNum1 = "InputNum1"
+      :InputNum2 = 'InputNum2'
+      @num3= 'getNum3'
+      />
+  </div>
+  
+</template>
+
+<script>
+import AppChild from './AppChild.vue'
+export default {
+  name: 'AppParent',
+  components:{
+    AppChild
+  },
+  data(){
+    return{
+      InputNum2: null,
+      InputNum3: null,
+    }
+  },
+  props:{
+    InputNum1: Number,
+  },
+  methods:{
+    getNum3(input){
+      // console.log(input)
+      this.InputNum3 = input
+    },
+    funcNum23: function(){
+      const context = {
+        InputNum3: this.InputNum3, 
+        InputNum2: this.InputNum2, 
+      }
+      this.$emit('num23',context)
+    }
+  }
+  
+
+}
+</script>
+
+<style>
+
+</style>
